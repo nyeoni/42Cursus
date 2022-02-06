a1f6003eda1bd2ac41736cd7e694372af5ef75c7
Chloekkk <skdusdl8804@gmail.com>
2022-02-06 18:06:59 +0900

    norm : main.c utils.c

--- pipex/src/main.c
+++ pipex/src/main.c
@@ -6,64 +6,66 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 02:35:25 by nkim              #+#    #+#             */
-/*   Updated: 2022/02/06 15:52:54 by nkim             ###   ########.fr       */
+/*   Updated: 2022/02/06 18:06:27 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void child_process(char *infile, char *cmd1, int *pipe_fd, char **envp)
+void	child_process(char *infile, char *cmd1, int *pipe_fd, char **envp)
 {
-	int fd;
+	int	fd;
 
 	fd = open(infile, O_RDONLY, 0777);
 	if (fd == -1)
-		throw_error("file open error");
+		throw_error("file open error", 1);
 	if (dup2(fd, STDIN_FILENO) == -1)
-		throw_error("redirection error");
+		throw_error("redirection error", 1);
 	if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
-		throw_error("redirection error");
+		throw_error("redirection error", 1);
 	close(pipe_fd[0]);
 	exec_cmd(cmd1, envp);
 }
 
-void parent_process(char *outfile, char *cmd2, int *pipe_fd, char **envp)
+void	parent_process(char *outfile, char *cmd2, int *pipe_fd, char **envp)
 {
-	int fd;
+	int	fd;
 
 	fd = open(outfile, O_WRONLY | O_TRUNC | O_CREAT, 0777);
 	if (fd == -1)
 		perror("file open error");
 	if (dup2(pipe_fd[0], STDIN_FILENO) == -1)
-		throw_error("redirection error");
+		throw_error("redirection error", 1);
 	if (dup2(fd, STDOUT_FILENO) == -1)
-		throw_error("redirection error");
+		throw_error("redirection error", 1);
 	close(pipe_fd[1]);
 	exec_cmd(cmd2, envp);
+	close(pipe_fd[0]);
 }
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
-	int pipe_fd[2];
-	int status;
-	pid_t pid;
-	pid_t wait_pid;
+	int		pipe_fd[2];
+	int		status;
+	pid_t	pid;
+	pid_t	wait_pid;
 
 	if (argc != 5)
-		throw_error("wrong arguments");
+		throw_error("wrong arguments", 1);
 	else
 	{
 		if (pipe(pipe_fd) == -1)
-			throw_error("pipe error");
-		pid = fork(); // 순서의 중요성 ^_^ 개빡치네
+			throw_error("pipe error", 1);
+		pid = fork();
 		if (pid == 0)
 			child_process(argv[1], argv[2], pipe_fd, envp);
 		else if (pid > 0)
 		{
-			wait_pid = waitpid(pid, &status, 0);
+			wait_pid = waitpid(pid, &status, WNOHANG);
 			parent_process(argv[4], argv[3], pipe_fd, envp);
 		}
 		else
-			throw_error("process fork fail");
+			throw_error("process fork fail", 1);
 	}
+	return (0);
 }
--- pipex/src/utils.c
+++ pipex/src/utils.c
@@ -6,51 +6,51 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 20:34:41 by nkim              #+#    #+#             */
-/*   Updated: 2022/02/06 15:16:25 by nkim             ###   ########.fr       */
+/*   Updated: 2022/02/06 18:06:14 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char *find_path(char *cmd, char **envp)
+char	*find_path(char *cmd, char **envp)
 {
-	char *PATH;
-	char *path;
-	char **path_list;
+	char	*PATH;
+	char	*path;
+	char	**path_list;
 
 	while (*envp)
 	{
 		if (!ft_strncmp(*envp, "PATH", 4))
-			break;
+			break ;
 		envp++;
 	}
-	PATH = ft_strchr(*envp, '=')  + 1;
+	PATH = ft_strchr(*envp, '=') + 1;
 	path_list = ft_split(PATH, ':');
 	while (*path_list)
 	{
 		path = ft_strjoin(*path_list, "/");
 		path = ft_strjoin(path, cmd);
 		if (!access(path, F_OK))
-			return path;
+			return (path);
 		path_list++;
 	}
-	return NULL;
+	return (NULL);
 }
 
 void	exec_cmd(char *cmd, char **envp)
 {
-	char *path;
-	char **cmds;
+	char	*path;
+	char	**cmds;
 
 	cmds = ft_split(cmd, ' ');
 	path = find_path(cmds[0], envp);
-	if (path)
-		execve(path, cmds, envp);
-	else
-		throw_error("the location of the command could not be found");
+	if (!path)
+		throw_error("command not found", 127);
+	if (execve(path, cmds, envp) == -1)
+		throw_error(0, 126);
 }
 
-void	throw_error(char *message)
+void	throw_error(char *message, int status)
 {
 	if (message)
 	{
@@ -64,5 +64,5 @@ void	throw_error(char *message)
 		ft_putstr_fd(strerror(errno), 2);
 		ft_putstr_fd("\n\033[0m", 2);
 	}
-	exit(EXIT_FAILURE);
+	exit(status);
 }

2f43521b09bd206f09b929c2da4618555434abcf
Chloekkk <skdusdl8804@gmail.com>
2022-02-10 19:40:01 +0900

    add : pipex

---
+++ pipex/include/pipex.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/03 02:36:53 by nkim              #+#    #+#             */
+/*   Updated: 2022/02/06 18:10:32 by nkim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../libft/include/libft42.h"
+# include <errno.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+void	exec_cmd(char *cmd, char **envp);
+void	throw_error(char *message, int status);
+
+#endif
---
+++ pipex/include_bonus/pipex_bonus.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/03 02:36:53 by nkim              #+#    #+#             */
+/*   Updated: 2022/02/06 18:10:23 by nkim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
+
+# include "../libft/include/libft42.h"
+# include <errno.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+void	exec_cmd(char *cmd, char **envp);
+void	throw_error(char *message, int status);
+
+#endif

b5beaecd4379ee09cba9df3d549a43ebadf3faa9
Chloekkk <skdusdl8804@gmail.com>
2022-02-24 03:02:33 +0900

    feat : rotate stack function

--- push_swap/src/operator/rotate.c
+++ push_swap/src/operator/rotate.c
@@ -6,8 +6,22 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 01:48:48 by nkim              #+#    #+#             */
-/*   Updated: 2022/02/24 01:48:58 by nkim             ###   ########.fr       */
+/*   Updated: 2022/02/24 02:39:24 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void rotate(t_stack *stack)
+{
+	t_list *head;
+
+	if (stack->len < 2)
+		return ;
+	head = stack->head;
+
+	stack->head = stack->head->next;
+	stack->tail->next = head;
+	stack->tail = head;
+	stack->tail = NULL;
+}

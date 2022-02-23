29a01641356561375377012e28ac65396f8de8d2
Chloekkk <skdusdl8804@gmail.com>
2022-02-24 02:32:28 +0900

    feat : stack swap top 2 elementes function

--- push_swap/src/operator/swap.c
+++ push_swap/src/operator/swap.c
@@ -1,14 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   operator.c                                         :+:      :+:    :+:   */
+/*   swap.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 01:47:12 by nkim              #+#    #+#             */
-/*   Updated: 2022/02/24 01:47:37 by nkim             ###   ########.fr       */
+/*   Updated: 2022/02/24 02:23:55 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void swap(t_stack *stack)
+{
+	t_list *a;
+	t_list *b;
+
+	if (stack->len < 2)
+		return ;
+	else if (stack->len == 2)
+		stack->tail = stack->head;
+	a = stack->head;
+	b = stack->head->next;
+	stack->head = a->next;
+	stack->head->next = a;
+	stack->head->next->next = b->next;
+}

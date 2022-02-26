bcc8bd3789f36756ebdc88f7a0afbf3cee0c2e44
Chloekkk <skdusdl8804@gmail.com>
2022-02-26 22:51:55 +0900

    update : update push_swap header files to add sort functions

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 16:57:23 by nkim              #+#    #+#             */
-/*   Updated: 2022/02/24 21:22:59 by nkim             ###   ########.fr       */
+/*   Updated: 2022/02/26 22:21:47 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,16 @@
 
 // 나중에 지워줄 것
 # include <stdio.h>
+
+#define SMALL 0
+#define LARGE 1
+
+#define RA_CNT 0
+#define PB_CNT 1
+#define RB_CNT 2
+
+#define TRUE 1
+#define FALSE 0
 
 typedef struct s_list t_list;
 
@@ -41,19 +51,29 @@ typedef struct s_stacks {
 }	t_stacks;
 
 /* STACK */
-void print_stacks(t_stacks *stacks);
 void init_stacks(t_stacks *stacks, int argc, char **argv);
 
 /* SORT */
-void sort_a_to_b(t_stacks *stacks);
+void select_pivot(int pivot[2], t_stack stack, int len);
+void sort_a_to_b(t_stacks *stacks, int cnt);
+void sort_three_or_under(t_stacks *stacks, t_stack *stack, int len);
+int is_ascending(t_stack *stack, int len);
+int is_descending(t_stack *stack, int len);
 
 /* OPERATOR */
-void swap(t_stack *stack);
-void push(t_stack *from, t_stack *to);
-void rotate(t_stack *stack);
-void reverse_rotate(t_stack *stack);
+void operator(char *op, t_stacks *stacks);
+int swap(t_stack *stack);
+int push(t_stack *from, t_stack *to);
+int rotate(t_stack *stack);
+int reverse_rotate(t_stack *stack);
 
 /* UTILS */
 void	throw_error();
+
+/* TEST */
+void print_stacks(t_stacks *stacks);
+void print_all_node(t_stack *stack);
+void test_all_op(t_stacks *stacks);
+
 
 #endif

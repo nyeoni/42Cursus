21eee81b126f9dde833febebd0b4ce0e7226e0fa
Chloekkk <skdusdl8804@gmail.com>
2022-02-27 18:58:37 +0900

    test : change test code

--- push_swap/src/__test__/test.c
+++ push_swap/src/__test__/test.c
@@ -6,7 +6,7 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 22:39:26 by nkim              #+#    #+#             */
-/*   Updated: 2022/02/25 00:29:02 by nkim             ###   ########.fr       */
+/*   Updated: 2022/02/26 23:45:25 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,12 @@ void test_all_op(t_stacks *stacks)
 
 	push(&stacks->a, &stacks->b);
 	printf("pb");
+	print_stacks(stacks);
+	print_all_node(&stacks->a);
+	print_all_node(&stacks->b);
+
+	push(&stacks->b, &stacks->a);
+	printf("pa");
 	print_stacks(stacks);
 	print_all_node(&stacks->a);
 	print_all_node(&stacks->b);

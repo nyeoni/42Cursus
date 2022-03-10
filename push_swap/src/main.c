b24fb9e8503ae4a99dd9a45c1747b8ec60002381
Chloekkk <skdusdl8804@gmail.com>
2022-03-10 16:04:13 +0900

    update : main update

--- push_swap/src/main.c
+++ push_swap/src/main.c
@@ -6,7 +6,7 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 16:57:10 by nkim              #+#    #+#             */
-/*   Updated: 2022/03/06 18:55:50 by nkim             ###   ########.fr       */
+/*   Updated: 2022/03/10 14:23:12 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	main(int argc, char **argv)
 {
-	t_stacks stacks;
+	t_stacks	stacks;
 
 	init_stacks(&stacks, argc, ++argv);
 	if (stacks.a.len <= 3)
@@ -25,13 +25,13 @@ int	main(int argc, char **argv)
 		sort_five(&stacks);
 	else
 		sort_a_to_b(&stacks, stacks.a.len);
-	free_stacks(&stacks);
+
 	// print_stacks(&stacks);
+	free_stacks(&stacks);
 	// if (is_ascending(&stacks.a, stacks.a.len))
 	// 	printf("OK\n");
 	// else
 	// 	printf("KO\n");
-
 	// free 하는거랑
 	// validate
 }

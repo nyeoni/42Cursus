aaca85070c6541fb17c056b197ee0e604d2beeac
Chloekkk <skdusdl8804@gmail.com>
2022-04-13 19:51:05 +0900

    fix: Update ascending

--- push_swap/src/main.c
+++ push_swap/src/main.c
@@ -6,7 +6,7 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 16:57:10 by nkim              #+#    #+#             */
-/*   Updated: 2022/03/26 14:18:09 by nkim             ###   ########.fr       */
+/*   Updated: 2022/04/13 19:49:20 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	main(int argc, char **argv)
 	if (argc == 1)
 		return (1);
 	init_stacks(&stacks, argc, ++argv);
+	if (is_ascending(&stacks.a, stacks.a.len))
+		return (0);
 	if (stacks.a.len <= 3)
 		sort_three_or_under(&stacks, stacks.a.len);
 	else if (stacks.a.len == 4)
